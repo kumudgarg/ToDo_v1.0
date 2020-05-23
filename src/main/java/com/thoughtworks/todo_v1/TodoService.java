5908bb80b58dff61e3958723750bd8116a130cc4
kumud garg <kumud.garg@kumuds-MacBook-Pro.local>
2020-05-24 02:02:34 +0530

    Refactored:1.1: added code to todoservice to get all todos

--- src/main/java/com/thoughtworks/todo_v1/TodoService.java
+++ src/main/java/com/thoughtworks/todo_v1/TodoService.java
@@ -1,10 +1,12 @@
 package com.thoughtworks.todo_v1;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class TodoService {
 
     @Autowired
